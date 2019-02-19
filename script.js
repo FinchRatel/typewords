ddac873e9fd940ef0361f5816a944e58e081a0a8
zhanghao30 <zhanghao30@hikvision.com.cn>
2019-02-19 18:17:39 +0800

    update

--- script.js
+++ script.js
@@ -1,107 +1,108 @@
-window.onload = function() {
-	var game = {
+window.onload = () => {
+	const game = {
 		dropSpeed: 1,
 		seconds: 0,
 		count: 0,
 		rtimes: 0,
 		wtimes: 0,
+		time: null,
 		screenWidth: parseInt(document.body.clientWidth),
-		screenHeight: parseInt(window.screen.availHeight),
-		wordsArr: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
-		$: function(className) {
-			return document.getElementsByClassName(className)[0];
+		screenHeight: parseInt(document.body.clientHeight),
+		wordsArr: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+		$(className) {
+			return document.querySelector('.' + className);
 		},
-		playRightAudio: function() {
+		playRightAudio() {
 			this.$("right").load();
 			this.$("right").play();
 			this.rtimes++;
 		},
-		playWrongAudio: function() {
+		playWrongAudio() {
 			this.$("wrong").load();
 			this.$("wrong").play();
 			this.wtimes++;
 		},
-		setBgMusicVolume: function() {
+		setBgMusicVolume() {
 			// this.$("bgmusic").play();
 			this.$("bgmusic").volume = 0.6;
 		},
-		checkWord: function(word) {
-			var self = this;
+		checkWord(word) {
 			word = word.toUpperCase();
-			for (var i = 0; i < self.domArr.length; i++) {
-				if (self.domArr[i].innerHTML == word) {
-					self.disappear(i);
-					self.playRightAudio();
+			for (let i = 0; i < this.domArr.length; i++) {
+				if (this.domArr[i].innerHTML == word) {
+					this.disappear(i);
+					this.playRightAudio();
 					return;
 				}
 			}
 			this.playWrongAudio();
 		},
-		keyDown: function() {
-			var self = this;
-			document.onkeydown = function(event) {
-				var e = event || window.event || arguments.callee.caller.arguments[0];
-				if (e&& e.keyCode) {
+		keyDown() {
+			document.onkeydown = (event) => {
+				let e = event || window.event || arguments.callee.caller.arguments[0];
+				if (e && e.keyCode) {
 					console.log(e.keyCode);
-					console.log(String.fromCharCode(e.keyCode));
-					if ((e && e.keyCode == 27)||(e && e.keyCode == 13)) {
+					console.log(String.fromCodePoint(e.keyCode));
+					if ((e && e.keyCode == 27) || (e && e.keyCode == 13)) {
 						alert("暂停")
 					} else {
-						self.checkWord(String.fromCharCode(e.keyCode))
+						this.checkWord(String.fromCodePoint(e.keyCode))
 					} 
 				}
 			};
 		},
-		add: function() {
-			var self = this;
-			setInterval(function() {
-				var temWord = self.wordsArr[Math.floor(Math.random() * 26)];
-				var temButton = document.createElement("button");
+		add() {
+			setInterval(() => {
+				let temWord = this.wordsArr[Math.floor(Math.random() * 26)];
+				let temButton = document.createElement("button");
 				temButton.innerHTML = temWord;
-				self.$("container").appendChild(temButton);
+				this.$("container").appendChild(temButton);
 			}, 1000);
 		},
-		update: function() {
-			var self = this;
-			setInterval(function() {
-				self.domArr = document.getElementsByTagName('button');				
-				for (var i = 0; i < self.domArr.length; i++) {
-					if (!self.domArr[i].style.top) {
-						var randNum = parseInt(Math.random() * self.screenWidth * 0.6 + self.screenWidth * 0.2);
-						self.domArr[i].style.top = "-50px";
-						self.domArr[i].style.left = randNum + "px";
+		update() {
+			setInterval(() => {
+				this.domArr = document.getElementsByTagName('button');				
+				for (let i = 0; i < this.domArr.length; i++) {
+					if (!this.domArr[i].style.top) {
+						let randNum = parseInt(Math.random() * this.screenWidth * 0.6 + this.screenWidth * 0.2);
+						this.domArr[i].style.top = "-50px";
+						this.domArr[i].style.left = randNum + "px";
 					} else {
-						if (parseInt(self.domArr[i].style.top) > self.screenHeight * 0.8) {
-							self.$("container").removeChild(self.domArr[i]);
+						if (parseInt(this.domArr[i].style.top) > this.screenHeight - 50) {
+							this.$("container").removeChild(this.domArr[i]);
+						} else {
+							this.domArr[i].style.top = parseInt(this.domArr[i].style.top) + this.dropSpeed + "px";
 						}
-						self.domArr[i].style.top = parseInt(self.domArr[i].style.top) + self.dropSpeed + "px";
 					}
 				}
 			}, 10)
 		},
-		disappear: function(i) {
-			// var self = this;
-			// domArr[i].style.animation = "myfirst 1s";
+		disappear(i) {
 			this.$("container").removeChild(this.domArr[i]);
-			// domArr[i].className = "destroy";
-
-			// self.container.removeChild(domArr[i]);
 		},
-		showInfo: function() {
-			var self = this;
-			setInterval(function() {
-				var temNow = new Date();
-				self.seconds = (temNow.getTime() - self.time.getTime()) / 1000;
-				self.$("time").innerHTML = (self.seconds).toFixed(3) + "S";
-				self.$("rtimes").innerHTML = self.rtimes;
-				self.$("wtimes").innerHTML = self.wtimes;
-				self.$("alltimes").innerHTML = self.wtimes + self.rtimes;
-				self.$("rpersent").innerHTML = (self.wtimes + self.rtimes) == 0 ? 0 : (self.rtimes / (self.wtimes + self.rtimes)).toFixed(3);
-				self.$("clickpersecond").innerHTML = ((self.wtimes + self.rtimes) / self.seconds).toFixed(3);
-				self.$("clickperminute").innerHTML = (60 * (self.wtimes + self.rtimes) / self.seconds).toFixed(3);
+		showInfo() {
+			setInterval(() => {
+				let temNow = new Date();
+				this.seconds = (temNow.getTime() - this.time.getTime()) / 1000;
+				this.$("time").innerHTML = (this.seconds).toFixed(3) + "S";
+				this.$("rtimes").innerHTML = this.rtimes;
+				this.$("wtimes").innerHTML = this.wtimes;
+				this.$("alltimes").innerHTML = this.wtimes + this.rtimes;
+				this.$("rpersent").innerHTML = (this.wtimes + this.rtimes) == 0 ? 0 : (this.rtimes / (this.wtimes + this.rtimes)).toFixed(3);
+				this.$("clickpersecond").innerHTML = ((this.wtimes + this.rtimes) / this.seconds).toFixed(3);
+				this.$("clickperminute").innerHTML = (60 * (this.wtimes + this.rtimes) / this.seconds).toFixed(3);
 			}, 10);
 		},
-		start: function() {
+		resize() {
+			window.onresize = () => {
+				this.screenWidth = parseInt(document.body.clientWidth);
+				this.screenHeight = parseInt(document.body.clientHeight);
+			};
+			window.onblur = () => {
+			};
+		},
+		start() {
+			this.resize();
 			this.time = new Date();
 			this.setBgMusicVolume();
 			this.showInfo();
